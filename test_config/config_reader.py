095115cc2c0a921a661e96cd32dcd9ef3d73b983
Iuliia603 <128023944+Iuliia603@users.noreply.github.com>
2023-09-12 11:55:25 -0400

    Create config_reader.py

---
+++ test_config/config_reader.py
@@ -0,0 +1,20 @@
+from config.json_file_reader import JsonFileReader
+from config.ini_file_reader import IniFileReader
+
+
+class ConfigReader:
+    def __init__(self, filename):
+        self.reader = None
+
+        if filename.endswith(".json"):
+            self.reader = JsonFileReader(filename)
+        elif filename.endswith(".ini"):
+            self.reader = IniFileReader(filename)
+        else:
+            raise Exception("File format is not supported")
+
+    def get_browser(self):
+        return self.reader.get_browser()
+
+    def get_wait_time(self):
+        return self.reader.get_wait_time()
